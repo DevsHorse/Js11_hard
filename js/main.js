6f146a5344059f366c7175a7d69e840420724a0d
Vladislav Holovko <lobsterponi@gmail.com>
2020-02-21 23:29:03 +0200

    added eng comments

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const searchPolindrome = (gridedString, median) => {
     const length = (gridedString.length) - 1;
     let i = median - 1;
 
-    //Перебор, сравнение боковых значений от каждой медианы. 
+    // Enumeration, comparison of lateral values from each median.
     for ( let j = median + 1; i > 0 && j < length; i--, j++ ) {
        if ( gridedString[i] !== gridedString[j] ) {
           return median - i;
@@ -17,21 +17,22 @@ const searchPolindrome = (gridedString, median) => {
   
 const whatLongestPolindrome = str => {
     const gridString = ['#'];
-    //Берем строку, и добавляем с обоих сторон символов решетку
+    // Take a line, and add a grid on both sides of the characters
     for ( let i = 0; i < str.length; i++ ) {
         if ( str[i] !== ' ' ) {
             gridString.push(str[i], '#');
         }
     }
 
-    // Вычисляем радиусы всех возможных полиндромов от каждого символа
+    // Calculate the radii of all possible palindromes from each character
     const radius = [];
     for ( let i = 0; i < gridString.length; i++ ) {
         radius[i] = searchPolindrome(gridString, i);
     }
 
     const writeFinalString = () => {
-        //Находим индекс и радиус медианы самого длинного полиндрома
+
+        // Find the index and median radius of the longest palindrome
         let count = 0;
         let index = 0;
         let newString = '';
@@ -45,20 +46,20 @@ const whatLongestPolindrome = str => {
             }
         });
 
-        //Записываем левую сторону от мединаы в строку
+        // Write the left side of the medina to the line
         for ( let i = index - 1; i > index - count; i-- ) {
             newString += gridString[i];
         }
 
-        //Переворот первой строки
+        // Flip The First Line
         newString = newString.split('').reverse().join('');
 
-        //Добавляем медиану + правую сторону от медианы
+        // Add the median and the right side of the median
         for ( let i = index; i < index + count; i++ ) {
             newString += gridString[i];
         }
 
-        //Убираем решетки, приводим строку в нормальный вид 
+        // Remove the lattice, bring the string to normal
         let longestPolindrome = '';
 
         newString.split('').forEach( item => {
